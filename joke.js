21593267b636759659f49d3a2da6eff1c1797457
HarkiratDhill <Harkirat.Dhillon@edu.sait.ca>
2025-03-23 14:51:51 -0600

    Button ID/Joke
    I made the button id's for JavaScript and have started working on the button function for the programming joke

--- joke.js
+++ joke.js
@@ -6,3 +6,20 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
     random.textContent = data.joke;
 }
 )
+
+const Programming = document.getElementById("Programming");
+const Misc = document.getElementById("Misc");
+const Pun = document.getElementById("Pun");
+const Spooky = document.getElementById("Spooky");
+const Christmas = document.getElementById("Christmas");
+
+fetch(" https://v2.jokeapi.dev/joke/Programming?safe-mode")
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    if (Programming.addEventListener('click', funciton(){
+
+    })) {
+        
+    }
+})
