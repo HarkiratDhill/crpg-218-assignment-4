0f44004ac2efbfbd330cc7e71aefa35388a693d3
HarkiratDhill <Harkirat.Dhillon@edu.sait.ca>
2025-03-26 21:37:00 -0600

    Title Change/Jokes
    I added a id to the h1 one tag on the html in order to make it easier for me to call to it when the title changes based on the button pressed. I also added the rest of the joke types to the JavaScript for the rest of the buttons.

--- joke.js
+++ joke.js
@@ -9,9 +9,10 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
     } else if (data.type === "twopart") {
       console.log(data.setup);  
       console.log(data.delivery); 
-      random.textContent = data.setup + ".." + data.delivery;
+      random.textContent = data.setup + " ... " + data.delivery;
     } 
   })
+  
   //Button Ids
   const Programming = document.getElementById("Programming");
   const Pun = document.getElementById("Pun");
@@ -19,6 +20,10 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
   const Spooky = document.getElementById("Spooky");
   const Christmas = document.getElementById("Christmas");
   
+  //h1 title
+  const title = document.getElementById("title");
+  
+  //Programming Joke JS
   fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")
   .then(response => response.json())
   .then(data => {
@@ -27,10 +32,88 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
         if (data.type === "single") {
             console.log(data.joke);
             random.textContent = data.joke;
+            title.innerText = "Random Programming Joke";
         } else if (data.type === "twopart") {
             console.log(data.setup);
             console.log(data.delivery);
-            random.textContent = data.setup + ".." + data.delivery;
+            random.textContent = data.setup + " ... " + data.delivery;
+            title.innerText = "Random Programming Joke";
         }
     })
   })
+  
+  //Misc Joke
+  fetch("https://v2.jokeapi.dev/joke/Misc?safe-mode")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    Misc.addEventListener('click', function(){
+        if (data.type === "single") {
+            console.log(data.joke);
+            random.textContent = data.joke;
+            title.innerText = "Random Misc Joke";
+        } else if (data.type === "twopart") {
+            console.log(data.setup);
+            console.log(data.delivery);
+            random.textContent = data.setup + " ... " + data.delivery;
+            title.innerText = "Random Misc Joke";
+        }
+    })
+  })
+
+  //Pun
+  fetch("https://v2.jokeapi.dev/joke/Pun?safe-mode")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    Pun.addEventListener('click', function(){
+        if (data.type === "single") {
+            console.log(data.joke);
+            random.textContent = data.joke;
+            title.innerText = "Random Pun";
+        } else if (data.type === "twopart") {
+            console.log(data.setup);
+            console.log(data.delivery);
+            random.textContent = data.setup + " ... " + data.delivery;
+            title.innerText = "Random Pun";
+        }
+    })
+  })
+
+  //Spooky Joke
+  fetch("https://v2.jokeapi.dev/joke/Spooky?safe-mode")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    Spooky.addEventListener('click', function(){
+        if (data.type === "single") {
+            console.log(data.joke);
+            random.textContent = data.joke;
+            title.innerText = "Random Spooky Joke";
+        } else if (data.type === "twopart") {
+            console.log(data.setup);
+            console.log(data.delivery);
+            random.textContent = data.setup + " ... " + data.delivery;
+            title.innerText = "Random Spooky Joke";
+        }
+    })
+  })
+
+    //Christmas Joke
+    fetch("https://v2.jokeapi.dev/joke/Christmas?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      Christmas.addEventListener('click', function(){
+          if (data.type === "single") {
+              console.log(data.joke);
+              random.textContent = data.joke;
+              title.innerText = "Random Christmas Joke";
+          } else if (data.type === "twopart") {
+              console.log(data.setup);
+              console.log(data.delivery);
+              random.textContent = data.setup + " ... " + data.delivery;
+              title.innerText = "Random Christmas Joke";
+          }
+      })
+    })
