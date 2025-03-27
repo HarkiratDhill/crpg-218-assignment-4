e4841b9c4ee008187981fb0dc6d5ac2828772961
HarkiratDhill <Harkirat.Dhillon@edu.sait.ca>
2025-03-26 21:11:21 -0600

    Fixed Random Joke/Made Button Joke
    I overhauled the JS code for the random generated joke to account for both single and twopart jokes. I also chose to make sure the button for the programming joke also and made sure to have the id's for the rest of the buttons be made, also having comments to make it clear which section is happening first.

--- joke.js
+++ joke.js
@@ -1,25 +1,36 @@
 fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
-.then(response => response.json())
-.then(data => {
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); 
+    // different joke types that are auto generated 
+    if (data.type === "single") {
+      console.log(data.joke);
+      random.textContent = data.joke;
+    } else if (data.type === "twopart") {
+      console.log(data.setup);  
+      console.log(data.delivery); 
+      random.textContent = data.setup + ".." + data.delivery;
+    } 
+  })
+  //Button Ids
+  const Programming = document.getElementById("Programming");
+  const Pun = document.getElementById("Pun");
+  const Misc = document.getElementById("Misc");
+  const Spooky = document.getElementById("Spooky");
+  const Christmas = document.getElementById("Christmas");
+  
+  fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")
+  .then(response => response.json())
+  .then(data => {
     console.log(data);
-    console.log(data.joke);
-    random.textContent = data.joke;
-}
-)
-
-const Programming = document.getElementById("Programming");
-const Misc = document.getElementById("Misc");
-const Pun = document.getElementById("Pun");
-const Spooky = document.getElementById("Spooky");
-const Christmas = document.getElementById("Christmas");
-
-fetch(" https://v2.jokeapi.dev/joke/Programming?safe-mode")
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    if (Programming.addEventListener('click', funciton(){
-
-    })) {
-        
-    }
-})
+    Programming.addEventListener('click', function(){
+        if (data.type === "single") {
+            console.log(data.joke);
+            random.textContent = data.joke;
+        } else if (data.type === "twopart") {
+            console.log(data.setup);
+            console.log(data.delivery);
+            random.textContent = data.setup + ".." + data.delivery;
+        }
+    })
+  })
