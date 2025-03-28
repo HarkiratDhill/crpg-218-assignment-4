b6d10b7ac0b05dbc1207fe683d5aee2d9039d7cb
HarkiratDhill <Harkirat.Dhillon@edu.sait.ca>
2025-03-28 09:34:17 -0600

    Fixed Errors
    I fixed some errors in the code that I was noticing and fixed it with back ticks

--- joke.js
+++ joke.js
@@ -1,4 +1,4 @@
-  fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
+  fetch(`https://v2.jokeapi.dev/joke/Any?safe-mode`)
   .then(response => response.json())
   .then(data => {
     console.log(data); 
@@ -14,29 +14,24 @@
   })
   
     function fetchJoke(category = "Any") {
-    const url = 'https://v2.jokeapi.dev/joke/${category}?safe-mode';
+    const url = `https://v2.jokeapi.dev/joke/${category}?safe-mode`;
     fetch(url)
     .then(response => response.json())
     .then(data => {
-      const joke = document.getElementById("joke");
-      const title = document.getElementById("title");
       console.log(data);
       if (data.type === "single") {
         joke.textContent = data.joke;
       } else if (data.type === "twopart") {
         console.log(data.setup);
         console.log(data.delivery);
-        joke.textContent = data.setup + "..." + data.delivery;
+        joke.textContent = `${data.setup} ... ${data.delivery}`;
       }
 
-      if (category === "Any") {
-        title.textContent = "joke of the day"
-      }
-      else if (category === "Pun") {
-          title.textContent = "random Pun"
+      if (category === "Pun") {
+          title.textContent = "Random Pun";
       }
       else {
-        title.textContent = 'random ${category} joke'
+        title.textContent = `Random ${category} Joke`;
       }
     })
   }
